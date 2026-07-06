ed80c8adce96295755adaea7f959e96cb519b3ba
Abu Huraira Ansari <ahurairaansari047@gmail.com>
2026-07-06 15:06:06 +0500

    updated the code

--- src/Components/Section1/Navbar.jsx
+++ src/Components/Section1/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between py-6 px-16'>
-      <h4>Target Audience</h4>
-      <button className='bg-black text-white px-5 py-4 rounded-full'>Digital Banking Platform</button>
+    <div className='flex items-center justify-between py-8 px-18'>
+      <h4 className='bg-black text-white px-6 py-3 rounded-full uppercase '>Target Audience</h4>
+      <button className='bg-gray-200 px-6 py-3 rounded-full uppercase font-semibold tracking-widest text-sm'>Digital Banking Platform</button>
     </div>
   )
 }
--- src/Components/Section1/Section1.jsx
+++ src/Components/Section1/Section1.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar"
+import Page1Content from "./Page1Content"
 
 const Section1 = ()=>{
     return(
         <div className="h-screen w-full">
             <Navbar/>
+            <Page1Content/>
         </div>
     )
 }
