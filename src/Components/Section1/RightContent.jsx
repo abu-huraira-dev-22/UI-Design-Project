f7432f822669504da39506b44cc7b94755b728ef
Abu Huraira Ansari <ahurairaansari047@gmail.com>
2026-07-07 14:48:11 +0500

    updated the code

--- src/Components/Section1/RightContent.jsx
+++ src/Components/Section1/RightContent.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+
 const RightContent = () => {
   return (
     <div className='h-full w-2/3 bg-blue-600'>
-      
+      <div>
+
+      </div>
+      <div>
+        <i class="ri-arrow-right-up-long-line"></i> 
+      </div>
     </div>
   )
 }
